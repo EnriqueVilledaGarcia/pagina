deb68c50309a0fecc535c3f0f4241fd2bec51d4b
Enrique Villeda Garcia <111399349+EnriqueVilledaGarcia@users.noreply.github.com>
2023-09-21 20:08:13 -0600

    Update login.php

--- login.php
+++ login.php
@@ -20,11 +20,37 @@ if ($conn->connect_error) {
 $consulta = "INSERT INTO datos (usuario, contraseña) VALUES ('$usuario', '$password')";
 
 if ($conn->query($consulta) === TRUE) {
-    echo "Registro exitoso";
+    echo '<div class="success-message">Registro exitoso</div>';
 } else {
-    echo "Error al registrar: " . $conn->error;
+    echo '<div class="error-message">Error al registrar: ' . $conn->error . '</div>';
 }
 
 // Cerrar la conexión a la base de datos
 $conn->close();
 ?>
+
+<?php
+echo '<style>
+.success-message {
+    background-color: #4CAF50; /* Color de fondo verde para mensaje de éxito */
+    color: white; /* Color de texto blanco */
+    padding: 10px; /* Espaciado interno */
+    border-radius: 5px; /* Bordes redondeados */
+    text-align: center; /* Alineación de texto al centro */
+    font-weight: bold; /* Texto en negrita */
+}
+
+.error-message {
+    background-color: #f44336; /* Color de fondo rojo para mensaje de error */
+    color: white; /* Color de texto blanco */
+    padding: 10px; /* Espaciado interno */
+    border-radius: 5px; /* Bordes redondeados */
+    text-align: center; /* Alineación de texto al centro */
+    font-weight: bold; /* Texto en negrita */
+}
+
+      </style>';
+?>
+
+
+
